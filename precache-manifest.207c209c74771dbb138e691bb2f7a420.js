6e1eb93ac08d7592b70ba645d4b80eab61fddd2e
Michelle Tang <msmetang@gmail.com>
2019-10-18 13:11:22 -0700

    Updates

---
+++ precache-manifest.207c209c74771dbb138e691bb2f7a420.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fd6db8ce302df7b1e6b3ae0ae6911d",
+    "url": "/r6-water-quality/index.html"
+  },
+  {
+    "revision": "27e104c3799600622fcc",
+    "url": "/r6-water-quality/static/css/2.441e21e3.chunk.css"
+  },
+  {
+    "revision": "3b92b194496202af58b5",
+    "url": "/r6-water-quality/static/css/main.085e2a57.chunk.css"
+  },
+  {
+    "revision": "27e104c3799600622fcc",
+    "url": "/r6-water-quality/static/js/2.23b27ea4.chunk.js"
+  },
+  {
+    "revision": "3b92b194496202af58b5",
+    "url": "/r6-water-quality/static/js/main.bcc35ed4.chunk.js"
+  },
+  {
+    "revision": "bc31d9591675108b8ef0",
+    "url": "/r6-water-quality/static/js/runtime-main.ddb93b7d.js"
+  },
+  {
+    "revision": "8eb1795267717f34804408a792b13879",
+    "url": "/r6-water-quality/static/media/r6_boundary.8eb17952.geojson"
+  },
+  {
+    "revision": "a27f406d0910b5db7beba145e1acf240",
+    "url": "/r6-water-quality/static/media/sites.a27f406d.csv"
+  }
+]);
